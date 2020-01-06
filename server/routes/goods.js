55cebf6316aa878a79bcda9e18deac3b50aa59cc
hznhzn <13247666239@163.com>
2020-01-06 20:38:18 +0800

    注释

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var Goods = require('../models/goods');
+var Goods = require('../models/goods'); //拿到商品模型
 
 //连接MongoDB数据库
 mongoose.connect('mongodb://127.0.0.1:27017/dumall');
@@ -20,13 +20,13 @@ mongoose.connection.on("disconnected", function () {
 
 //查询商品列表数据
 router.get("/list", function (req,res,next) {
-  let page = parseInt(req.param("page"));
-  let pageSize = parseInt(req.param("pageSize"));
-  let priceLevel = req.param("priceLevel");
-  let sort = req.param("sort");
+  let page = parseInt(req.param("page")); // 页码
+  let pageSize = parseInt(req.param("pageSize"));// 一页几条数据
+  let priceLevel = req.param("priceLevel"); //价格区间
+  let sort = req.param("sort"); //价格升序/降序
   let skip = (page-1)*pageSize;
   var priceGt = '',priceLte = '';
-  let params = {};
+  let params = {}; //参数
   if(priceLevel!='all'){
     switch (priceLevel){
       case '0':priceGt = 0;priceLte=100;break;
@@ -34,100 +34,101 @@ router.get("/list", function (req,res,next) {
       case '2':priceGt = 500;priceLte=1000;break;
       case '3':priceGt = 1000;priceLte=5000;break;
     }
-    params = {
+    params = { //salePrice>priceGt且salePrice<=priceLte
       salePrice:{
-          $gt:priceGt,
-          $lte:priceLte
+        $gt:priceGt,
+        $lte:priceLte
       }
     }
   }
   let goodsModel = Goods.find(params).skip(skip).limit(pageSize);
   goodsModel.sort({'salePrice':sort});
   goodsModel.exec(function (err,doc) {
-      if(err){
-          res.json({
-            status:'1',
-            msg:err.message
-          });
-      }else{
-          res.json({
-              status:'0',
-              msg:'',
-              result:{
-                  count:doc.length,
-                  list:doc
-              }
-          });
-      }
+    if(err){
+      res.json({
+        status:'1',
+        msg:err.message
+      });
+    }else{
+      res.json({
+        status:'0',
+        msg:'',
+        result:{
+          count:doc.length,
+          list:doc
+        }
+      });
+    }
   })
 });
 
 //加入到购物车
 router.post("/addCart", function (req,res,next) {
-  var userId = '100000077',productId = req.body.productId;
-  var User = require('../models/user');
-  User.findOne({userId:userId}, function (err,userDoc) {
-    if(err){
-        res.json({
-            status:"1",
-            msg:err.message
-        })
+  var userId = '100000077' //用户id
+  var productId = req.body.productId //商品id
+  var User = require('../models/user') //拿到用户模型
+  User.findOne({userId:userId}, function (err,userDoc) {//查询用户信息
+    if(err){ // 如果出错，打印错误信息
+      res.json({
+        status:"1",
+        msg:err.message
+      })
     }else{
-        console.log("userDoc:"+userDoc);
-        if(userDoc){
-          var goodsItem = '';
-          userDoc.cartList.forEach(function (item) {
-              if(item.productId == productId){
-                goodsItem = item;
-                item.productNum ++;
-              }
-          });
-          if(goodsItem){
-            userDoc.save(function (err2,doc2) {
-              if(err2){
-                res.json({
-                  status:"1",
-                  msg:err2.message
-                })
-              }else{
-                res.json({
-                  status:'0',
-                  msg:'',
-                  result:'suc'
-                })
-              }
-            })
-          }else{
-            Goods.findOne({productId:productId}, function (err1,doc) {
-              if(err1){
-                res.json({
-                  status:"1",
-                  msg:err1.message
-                })
-              }else{
-                if(doc){
-                  doc.productNum = 1;
-                  doc.checked = 1;
-                  userDoc.cartList.push(doc);
-                  userDoc.save(function (err2,doc2) {
-                    if(err2){
-                      res.json({
-                        status:"1",
-                        msg:err2.message
-                      })
-                    }else{
-                      res.json({
-                        status:'0',
-                        msg:'',
-                        result:'suc'
-                      })
-                    }
-                  })
-                }
-              }
-            });
+      console.log("userDoc:"+userDoc);
+      if(userDoc){
+        var goodsItem = '';
+        userDoc.cartList.forEach(function (item) {// 判断购物车中是否有这件商品
+          if(item.productId == productId){// 如果购物车中有这件商品，则商品数量++即可
+            goodsItem = item;
+            item.productNum ++;
           }
+        });
+        if(goodsItem){// 如果购物车中有这件商品
+          userDoc.save(function (err2,doc2) {//保存到数据库
+            if(err2){
+              res.json({
+                status:"1",
+                msg:err2.message
+              })
+            }else{
+              res.json({
+                status:'0',
+                msg:'',
+                result:'suc'
+              })
+            }
+          })
+        }else{// 如果购物车中没有这件商品
+          Goods.findOne({productId:productId}, function (err1,doc) {//在商品列表中找到这个商品
+            if(err1){//如果没找到/出错
+              res.json({
+                status:"1",
+                msg:err1.message
+              })
+            }else{//如果找到了，把商品信息保存到购物车
+              if(doc){
+                doc.productNum = 1;//商品数量为1
+                doc.checked = 1; //商品默认为选中
+                userDoc.cartList.push(doc);
+                userDoc.save(function (err2,doc2) { //将商品信息保存进去购物车
+                  if(err2){
+                    res.json({
+                      status:"1",
+                      msg:err2.message
+                    })
+                  }else{
+                    res.json({
+                      status:'0',
+                      msg:'',
+                      result:'suc'
+                    })
+                  }
+                })
+              }
+            }
+          });
         }
+      }
     }
   })
 });
